5198d4d7ff52cfd110a31c6a97389a7fe8d34cd8
dlgmltjr0925 <dlgmltjr0925@gmail.com>
2020-11-10 22:38:08 +0900

    Write a summary of the 2178 problem

---
+++ baekjoon/2178/summary.md
@@ -0,0 +1,13 @@
+# Summary
+
+### Title
+
+- 미로 탐색
+
+### URL
+
+- https://www.acmicpc.net/problem/2178
+
+### Classification
+
+- BFS
--- utils/javascript.js
+++ utils/javascript.js
@@ -1,4 +1,20 @@
-exports.checkRunTime = async (callback, options = { label: callback.name, count: 100 }) => {
+exports.checkRunTime = async (callbacks, options = { count: 10 }) => {
+  for (const callback in callbacks) {
+    let count = options.count || 10;
+    let time = 0;
+    for (let i = 0 ;i < count; i++) {
+      const start = new Date();
+      await callback();
+      const end = new Date();
+      time += end.valueOf() - start.valueOf();
+      if (time > 10000) count = i + 1;
+    }
+
+    console.log(`${callback.name}: ${time / count}ms`)
+  }
+}
+
+exports.checkRunTime = async (callback, options = { label: callback.name, count: 10 }) => {
   const label = options.label || callback.name;
   let count = options.count || 10;
   let time = 0;
